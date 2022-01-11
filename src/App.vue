185a9dc76d2d79040f26e14470f5fa6409efb01e
Martyna Machowiak <martyna.machowiak@gmail.com>
2022-01-11 15:33:28 +0000

    Add LocationDetails

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LocationDetails />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LocationDetails from './components/LocationDetails.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    LocationDetails,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/LocationDetails.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h2>Location details</h2>
+    <p>Lorem ipsum</p>
+  </div>
+</template>
+
+<script>
+export default { name: 'LocationDetails' };
+</script>
