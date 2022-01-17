2f7b353e4a3c541687e6456974de479af25394dc
martyna.machowiak <martyna.machowiak@moneysupermarket.com>
2022-01-17 18:50:00 +0000

    WIP Add test file for ForecastDetails

---
+++ src/tests/ForecastDetails.test.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils';
+import ForecastDetails from '../components/ForecastDetails.vue';
+
+describe('ForecastDetails', () => {
+  it('should render', () => {
+    // const wrapper = shallowMount(ForecastDetails);
+
+    expect(2).toEqual(2);
+  });
+});
